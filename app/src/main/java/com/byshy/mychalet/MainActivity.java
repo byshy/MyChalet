aef9e4c128087bf3addbbd41b797b0e9ecc266c9
Basel <baselhadrous99@gmail.com>
2018-12-05 10:54:10 +0200

    Added Firebase Support

--- app/src/main/java/com/byshy/mychalet/MainActivity.java
+++ app/src/main/java/com/byshy/mychalet/MainActivity.java
@@ -2,12 +2,31 @@ package com.byshy.mychalet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mUpload;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUpload = findViewById(R.id.firebase);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("Admin_01").child("Name").setValue("Basel");
+            }
+        });
     }
+
 }
